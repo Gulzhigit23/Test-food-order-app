ae5b82de4306b9453971c55f6543952cf15b675d
Gulzhigit <guljigit.omurkulov@gmail.com>
2022-08-30 20:23:57 +0600

    created cart-component layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Header from './components/Layout/Header';
+import Header from './components/Header';
 
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import mealsImages from '../../assets/meals.jpeg'
-import classes from '../Header.module.css'
+import mealsImages from '../assets/meals.jpeg'
+import classes from './Header.module.css'
+import HeaderCardButton from './Layout/HeaderCardButton'
 function Header() {
   return (
     <>
         <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCardButton/>
         </header>
         <div className={classes['main-image']}>
         <img src={mealsImages} alt="food" />
