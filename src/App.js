a29552ebcf9ac5c596a58bc5bf51a2d5b1c7746a
Gulzhigit <guljigit.omurkulov@gmail.com>
2022-08-30 15:40:29 +0600

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
+import { useRef } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+const ref = useRef('red')
+
+
+useEffect(() => {
+  const currentDiv = ref.current.style
+  const timerColor = setInterval(() => {
+   currentDiv.background === 'red' 
+   ? (currentDiv.background = 'green')
+   : (currentDiv.background = 'red')
+
+  },1000)
+  return () => clearInterval(timerColor)
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+  
+  <div className='container' ref={ref}>
+
+  </div>
     </div>
   );
 }
 
+
 export default App;
+
+
